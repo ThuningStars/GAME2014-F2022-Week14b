95f1a2e4e55f1ab5ffd94bfaa19eac6bec2d9dcd
Wanbo W <2769324330@qq.com>
2022-11-19 00:30:34 -0500

    added Moving Platform

--- Assets/[Scripts]/MovingPlatformController.cs
+++ Assets/[Scripts]/MovingPlatformController.cs
@@ -1,6 +1,8 @@
+using Cinemachine.Utility;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class MovingPlatformController : MonoBehaviour
 {
@@ -15,19 +17,67 @@ public class MovingPlatformController : MonoBehaviour
     public float verticalDistance = 8.0f;
     [Range(1.0f, 20.0f)]
     public float verticalSpeed = 3.0f;
+    [Range(0.01f, 0.1f)]
+    public float customSpeedFactor = 0.02f;
+
+    [Header("Platform Path Points")]
+    public List<Transform> pathPoints;
 
     private Vector2 startPoint;
+    private Vector2 destinationPoint;
+    private List<Vector2> pathList;
+    private float timer;
+    private int currentPointIndex;
 
     // Start is called before the first frame update
     void Start()
     {
+        timer = 0.0f;
+        currentPointIndex = 0;
         startPoint = transform.position;
+        pathList = new List<Vector2>();     // create new container of vector2s
+
+        // copy each pathPoint Transform to the pathList and add startPoint
+        foreach (var pathPoint in pathPoints)
+        {
+            Vector2 point = new Vector2(pathPoint.localPosition.x + startPoint.x,
+                                    pathPoint.localPosition.y + startPoint.y);
+            pathList.Add(point);
+        }
+        pathList.Add(transform.position);
+
+        destinationPoint = pathList[currentPointIndex];
     }
 
     // Update is called once per frame
     void Update()
     {
         Move();
+    }
+
+    void FixedUpdate()
+    {
+        if(direction == PlatformDirection.CUSTOM)
+        {
+            if(timer <= 1.0f)
+            {
+                timer += customSpeedFactor;
+            }
+
+            if(timer >= 1.0f)
+            {
+                timer = 0.0f;
+
+                currentPointIndex++;
+                if(currentPointIndex >= pathList.Count)
+                {
+                    currentPointIndex = 0;
+                }
+
+                startPoint = transform.position;
+                destinationPoint = pathList[currentPointIndex];
+            }
+        }
     }
 
     public void Move()
@@ -54,10 +104,10 @@ public class MovingPlatformController : MonoBehaviour
                 transform.position = new Vector2(
                     Mathf.PingPong(horizontalSpeed * Time.time, horizontalDistance) + startPoint.x,
                     startPoint.y - Mathf.PingPong(verticalSpeed * Time.time, verticalDistance));
-
                 break;
 
             case PlatformDirection.CUSTOM:
+                transform.position = Vector2.Lerp(startPoint, destinationPoint, timer);
                 break;
         }
     }
